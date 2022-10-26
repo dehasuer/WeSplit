afa8cd5841fcb8ec2b582abf15b7ed6d95d57b2f
dehasuer <dehasuer97@gmail.com>
2022-10-26 15:56:19 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Deha Süer on 25.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
